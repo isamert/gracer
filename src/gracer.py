786e4a22a3143afac9468a87d5b2d22f61d375e0
isamert <isamertgurbuz@gmail.com>
2016-03-21 02:27:09 +0200

    little fixes, full settings page

--- src/gracer.py
+++ src/gracer.py
@@ -4,17 +4,61 @@ import subprocess
 from gi.repository import GObject, Gedit, Gtk, GtkSource, PeasGtk
 
 
+class Settings:
+    def get_config_file_path():
+        path = os.path.abspath(os.path.dirname(__file__))
+        path += os.path.sep
+        return path + "gracer.config"
+
+    def get_value(key, default_value):
+        if os.path.exists(Settings.get_config_file_path()):
+            settings = {}
+            with open(Settings.get_config_file_path(), 'r') as settings_file:
+                for line in settings_file.readlines():
+                    _key, val = line.rstrip('\n').split("=")
+                    settings[_key] = val
+
+            if key in settings:
+                return settings[key]
+        return default_value
+
+    def save_settings(settings):
+        with open(Settings.get_config_file_path(), 'w') as settings_file:
+            settings_file.writelines([key + "=" + settings[key] + '\n' for key in settings])
+
+
 class Racer:
-    def get_rust_src_path():
-        pass
+    def __init__(self):
+        self.RUST_SRC_PATH = None
+        self.RACER_PATH    = None
 
-    def get_racer_command():
-        #FIXME: RUST_SRC_PATH
-        #FIXME: check racer executable
-        paths = ("/usr/bin/racer", "/usr/local/bin/racer", os.path.expanduser("~/.cargo/bin/racer"))
-        return [path for path in paths if os.path.exists(path)][0]
+    def get_rust_src_path(self):
+        if self.RUST_SRC_PATH is not None:
+            return self.RUST_SRC_PATH
 
-    def init_racer(document, subcommand):
+        default_value = ""
+        possible_rust_paths = ("/usr/src/rust/src", "/usr/local/src/rust/src", os.environ.get('RUST_SRC_PATH'))
+        for path in possible_rust_paths:
+            if path is not None and os.path.exists(path):
+                default_value = path
+
+        self.RUST_SRC_PATH = Settings.get_value("rust_src_path", default_value)
+        return self.RUST_SRC_PATH
+
+    def get_racer_path(self):
+        if self.RACER_PATH is not None:
+            return self.RACER_PATH
+
+        default_value = ""
+        possible_racer_paths = ("/usr/bin/racer", "/usr/local/bin/racer", os.path.expanduser("~/.cargo/bin/racer"))
+        for path in possible_racer_paths:
+            if os.path.exists(path):
+                default_value = path
+
+        self.RACER_PATH = Settings.get_value("racer_path", default_value)
+        return self.RACER_PATH
+
+    def init_racer(self, document, subcommand):
         temp_file = tempfile.NamedTemporaryFile()
         doc_text = document.get_text(document.get_start_iter(), document.get_end_iter(), False)
         temp_file.write(doc_text.encode('utf-8'))
@@ -24,19 +68,25 @@ class Racer:
         linenum = iter_cursor.get_line() + 1
         charnum = iter_cursor.get_line_index()
 
-        proc_args = (Racer.get_racer_command(), subcommand, str(linenum), str(charnum), temp_file.name)
+        rust_src_path = self.get_rust_src_path()
+        racer_path = self.get_racer_path()
+
+        if not os.path.exists(rust_src_path) or not os.path.exists(racer_path):
+            print(rust_src_path, racer_path, "Try to set rust source path and racer path in plugin settings.")
+
+        proc_args = (racer_path, subcommand, str(linenum), str(charnum), temp_file.name)
         output = ""
 
         try:
-            output = subprocess.check_output(proc_args).decode()
+            output = subprocess.check_output(proc_args, env=dict(os.environ, RUST_SRC_PATH=rust_src_path)).decode()
         except Exception as e:
-            print(proc_args, e.args)
+            print(e)
 
         temp_file.close()
         return output
 
-    def get_matches(document):
-        proc_result = Racer.init_racer(document, "complete")
+    def get_matches(self, document):
+        proc_result = self.init_racer(document, "complete")
         if proc_result == "" or proc_result is None:
             return []
 
@@ -53,8 +103,8 @@ class Racer:
 
         return completion
 
-    def get_definition(document):
-        proc_result = Racer.init_racer(document, "find-definition")
+    def get_definition(self, document):
+        proc_result = self.init_racer(document, "find-definition")
         if proc_result == "" or proc_result is None:
             return None
 
@@ -76,6 +126,7 @@ class Racer:
 class GracerPlugin(GObject.Object, Gedit.ViewActivatable, PeasGtk.Configurable):
     __gtype_name__ = "GracerPlugin"
     view = GObject.property(type=Gedit.View)
+    racer = Racer()
 
     def __init__(self):
         GObject.Object.__init__(self)
@@ -89,7 +140,7 @@ class GracerPlugin(GObject.Object, Gedit.ViewActivatable, PeasGtk.Configurable):
         document.connect("load", self.on_document_load)
 
         if document.get_uri_for_display().endswith(".rs"):
-            self.completion_provider = GracerCompletionProvider(self.view)
+            self.completion_provider = GracerCompletionProvider(self.view, self.racer)
             self.view.get_completion().add_provider(self.completion_provider)
             self.view.connect('populate-popup', self.on_view_populate_popup)
 
@@ -100,7 +151,7 @@ class GracerPlugin(GObject.Object, Gedit.ViewActivatable, PeasGtk.Configurable):
     def on_document_load(self, document):
         if document.get_uri_for_display().endswith(".rs"):
             if self.completion_provider is None:
-                self.completion_provider = GracerCompletionProvider(self.view)
+                self.completion_provider = GracerCompletionProvider(self.view, self.racer)
                 self.view.get_completion().add_provider(self.completion_provider)
 
             if self.handler_on_view_populate_popup is None:
@@ -114,25 +165,40 @@ class GracerPlugin(GObject.Object, Gedit.ViewActivatable, PeasGtk.Configurable):
                 self.view.disconnect(self.handler_on_view_populate_popup)
                 self.handler_on_view_populate_popup = None
 
-            #self.view.connect('populate-popup', self.on_view_populate_popup)
-
     def do_create_configure_widget(self):
-        table = Gtk.Table(2, 2, Gtk.true())
+        table = Gtk.Table(3, 2, Gtk.true())
         table.set_row_spacings(5)
         table.set_col_spacings(5)
 
-        lbl_racerpath = Gtk.Label("Racer path:       ")
+        lbl_racerpath = Gtk.Label("Racer path: ")
         lbl_rustsrc   = Gtk.Label("Rust source path: ")
 
         self.txt_racerpath = Gtk.TextView()
-        self.txt_rustsrc = Gtk.TextView()
+        self.txt_rustsrc   = Gtk.TextView()
+
+        rust_src_path = self.racer.get_rust_src_path()
+        racer_path = self.racer.get_racer_path()
+
+        self.txt_racerpath.get_buffer().set_text(racer_path)
+        self.txt_rustsrc.get_buffer().set_text(rust_src_path)
+
+        btn_save = Gtk.Button("Save")
+        btn_save.connect("clicked", self.on_btn_save_clicked)
 
         table.attach(lbl_racerpath, 0, 1, 0, 1)
         table.attach(lbl_rustsrc, 0, 1, 1, 2)
         table.attach(self.txt_racerpath, 1, 2, 0, 1)
         table.attach(self.txt_rustsrc, 1, 2, 1, 2)
+        table.attach(btn_save, 0, 2, 2, 3)
 
         return table
+
+    def on_btn_save_clicked(self, btn):
+        settings = {"racer_path":self.txt_racerpath.get_buffer().get_text(self.txt_racerpath.get_buffer().get_start_iter(),
+                                                                             self.txt_racerpath.get_buffer().get_end_iter(), False),
+                    "rust_src_path":self.txt_rustsrc.get_buffer().get_text(self.txt_rustsrc.get_buffer().get_start_iter(),
+                                                                             self.txt_rustsrc.get_buffer().get_end_iter(), False)}
+        Settings.save_settings(settings)
 
     def on_view_populate_popup(self, view, menu):
         menu_find_definition = Gtk.ImageMenuItem(_("Find Definition"))
@@ -147,22 +213,24 @@ class GracerPlugin(GObject.Object, Gedit.ViewActivatable, PeasGtk.Configurable):
         menu.prepend(menu_find_definition)
 
     def on_find_definition_active(self, menu_item):
+        #FIXME: check file name
         document = self.view.get_buffer()
-        result = Racer.get_definition(document)
+        result = self.racer.get_definition(document)
         try:
             result_line = int(result[0]) - 1 # -1, because line numbering is 0-based
             result_char = int(result[1])
             document.place_cursor(document.get_iter_at_line_offset(result_line, result_char))
         except Exception as e: # probably not found anything
-            print(result, e.args)
+            print(e)
 
 
 class GracerCompletionProvider(GObject.Object, GtkSource.CompletionProvider):
     __gtype_name__ = 'GracerProvider'
 
-    def __init__(self, view):
+    def __init__(self, _view, _racer):
         GObject.Object.__init__(self)
-        self._view = view
+        self.view = _view
+        self.racer = _racer
 
     def do_get_name(self):
         return _("Gracer Rust Code Completion")
@@ -175,11 +243,11 @@ class GracerCompletionProvider(GObject.Object, GtkSource.CompletionProvider):
         return 0
 
     def do_populate(self, context):
-        document = self._view.get_buffer()
+        document = self.view.get_buffer()
         proposals = []
 
         #TODO: add completion for type (ex. add extra brackets for functions)
-        for _text, _type, _path in Racer.get_matches(document):
+        for _text, _type, _path in self.racer.get_matches(document):
             proposals.append(GtkSource.CompletionItem.new(_text, _text, self.get_icon_for_type(_type), _path))
 
         context.add_proposals(self, proposals, True)
